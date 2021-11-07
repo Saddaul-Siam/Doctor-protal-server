269905f7484110fd90f89dcf7d878c05721d5fd5
saddaul-siam <saddadul.siam@gmail.com>
2021-11-07 11:39:11 +0600

    added appointments api

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hg2sj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -14,13 +15,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     await client.connect();
-    console.log("database connected successfully");
-    // const database = client.db('sample_mflix');
-    // const movies = database.collection('movies');
-    // // Query for a movie that has the title 'Back to the Future'
-    // const query = { title: 'Back to the Future' };
-    // const movie = await movies.findOne(query);
-    // console.log(movie);
+    const database = client.db('doctors_protal');
+    const appointmentCollection = database.collection('appointments');
+
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      const query = { email: email, date: date }
+      const cursor = appointmentCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments)
+    })
+
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentCollection.insertOne(appointment)
+      res.json(result);
+    })
+
   } finally {
     // await client.close();
   }
@@ -28,9 +40,8 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello doctors protal server')
+  res.send('Hello doctors protal server!!!')
 })
-
 app.listen(port, () => {
   console.log(`listening at ${port}`)
 })
